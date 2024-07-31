574c33813f715f9309366eb6189bb8b83a49a7db
zhangdapeng520 <1156956636@qq.com>
2024-07-31 09:21:30 +0800

    新增：增加404统一错误信息返回

--- response_error.go
+++ response_error.go
@@ -6,30 +6,37 @@ import (
 	"net/http"
 )
 
-type SuccessResult struct {
+type ErrorResult struct {
 	Code    int         `json:"code"`
 	Status  bool        `json:"status"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data,omitempty"`
 }
 
-var successResult = SuccessResult{
-	Code:    10000,
-	Status:  true,
-	Message: "success",
+var errorResult = ErrorResult{
+	Code:    10500,
+	Status:  false,
+	Message: "server error",
 	Data:    nil,
 }
 
-// ResponseSuccess 统一返回类型，成功的返回
+// ResponseError404 统一返回类型，错误信息资源不存在的返回
 // @param w 网络输出流对象
 // @param data 要返回的内容
-func ResponseSuccess(w http.ResponseWriter, data interface{}) {
+func ResponseError404(w http.ResponseWriter, data interface{}) {
 	// 声明返回的是JSON数据
 	w.Header().Set("Content-Type", "application/json")
 
+	// 构造错误信息
+	result := ErrorResult{
+		Code:    10404,
+		Status:  false,
+		Message: "not found",
+		Data:    data,
+	}
+
 	// 将字段转换为JSON
-	jsonBytes, err := json.Marshal(successResult)
-	successResult.Data = data
+	jsonBytes, err := json.Marshal(result)
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 		return
